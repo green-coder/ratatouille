c86dda7c4252926ec3e67e673d80e8ba8fefe9dc
Vincent Cantin <vincent@404.taipei>
2019-04-02 23:16:23 +0800

    v0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ratatouille/lein-template "0.0.1-SNAPSHOT"
+(defproject ratatouille/lein-template "0.1.0"
   :description "A leiningen template"
   :url "https://github.com/green-coder/ratatouille"
   :license {:name "Eclipse Public License"
